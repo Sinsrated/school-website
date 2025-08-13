728ad5df0d92bd7f9a647ff95b069b085dead557
Sinsrated <ratedsinner@gmail.com>
2025-08-13 19:43:09 +0300

    launch

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,11 @@
 const path = require('path');
 
+
 module.exports = {
-  entry: './src/index.js',
   output: {
-    path: path.resolve( 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-    clean: true,
+    filename: 'school-website.js',
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-    ],
+    rules: [{test: /\.js$/, use: 'babel-loader'}],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    historyApiFallback: true,
-    port: 8080,
-    open: true,
-    hot: true,
-  },
-  mode: 'development',
-};
+} 
